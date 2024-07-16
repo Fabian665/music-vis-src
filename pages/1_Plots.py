ad8a58f4e8c6d373828ba51d9764296df0188ae8
Fabian665 <71074539+Fabian665@users.noreply.github.com>
2024-07-17 00:05:08 +0300

    Optimaizations

--- pages/1_Plots.py
+++ pages/1_Plots.py
@@ -26,7 +26,7 @@ def read_data():
     glz_df['year'] = glz_df['date'].dt.year
     return glz_df
 
-@st.cache_data()
+@st.cache_data
 def genre_mask(genre):
     ret = glz_df['artist_genres'].apply(predicate, args=(genre, ))
     return ret
@@ -34,7 +34,7 @@ def genre_mask(genre):
 def predicate(artist_genres, genre):
     return genre in artist_genres if isinstance(artist_genres, list) else False
 
-@st.cache_data()
+@st.cache_data
 def split_data(genres, split_feature, output_features):
     if split_feature == 'None':
         return {None: abs(glz_df[output_features].values)}
@@ -44,7 +44,7 @@ def split_data(genres, split_feature, output_features):
     genre_feature_values = {genre: abs(df[output_features].values) for df, genre in zip(dfs, genres)}
     return genre_feature_values
 
-@st.cache_data()
+@st.cache_data
 def get_distinct_songs(glz_df):
     distinct_songs = glz_df.sort_values(by='date').drop_duplicates(subset='track_name', keep='first')
     X = distinct_songs.date.apply(lambda x: x.timestamp()).values
@@ -52,50 +52,72 @@ def get_distinct_songs(glz_df):
     polynomial = Polynomial.fit(X, Y, 1)
     return distinct_songs, polynomial
 
-# @st.cache_data()
-# def get_polynomial(distinctint_songs):
-#     X = distinct_songs.date.apply(lambda x: x.timestamp()).values
-#     Y = distinct_songs.duration_dt.apply(lambda x: x.timestamp())
-#     return Polynomial.fit(X, Y, 1)
+@st.cache_data
+def get_artist_song_count(df):
+    artist_song_count = (
+        df.groupby("main_artist_name")
+        #   .agg(unique_tracks='nunique', chart_appearances='count')
+        .agg({
+            'main_artist': 'first',
+            'track_name': ['nunique', 'count'],
+        })
+        .reset_index()
+    )
+    artist_song_count.columns = ['Artist', 'main_artist', 'unique_tracks', 'chart_appearances']
+    artist_song_count['ratio'] = artist_song_count.apply(lambda x: x['chart_appearances'] / x['unique_tracks'], axis=1)
+    del(artist_song_count['chart_appearances'])
+    return artist_song_count
 
-@st.cache_data()
-def get_top_10_artists(market):
-    market_data = filter_by_market(market)
-    # Calculate the number of weeks each song stays in the top 10
-    market_data['week_count'] = market_data.groupby('track_name')['date'].transform(lambda x: x.nunique())
-    # Calculate the total number of appearances for each artist
-    artist_appearances = market_data.groupby('main_artist').size().reset_index(name='count')
-    return artist_appearances.nlargest(10, 'count')['main_artist']
+@st.cache_data
+def filter_dataframe(df, *args):
+    filtered_df = df.copy()
+
+    # Iterate through the filter arguments
+    mask = np.all([get_filter_mask(df, arg) for arg in args], axis=0)
+    filtered_df = filtered_df[mask]
+
+    return filtered_df
+
+@st.cache_data
+def get_filter_mask(df, arg):
+    col_name, criterion = arg
+    if col_name == 'date':
+        return (df['date'] >= criterion[0]) & (df['date'] <= criterion[1])
+    elif col_name == 'rank':
+        return df['rank'] <= criterion
+    elif col_name == 'market':
+        if criterion in ['IL', 'INTL']:
+            return df['market'] == criterion
+        else:
+            return np.ones(len(df), dtype=bool)
+    else:
+        return df[col_name] == criterion
 
 @st.cache_data
 def search_track(song_name, artist_name=None, album_name=None):
     return st.session_state.spotify.search_track(song_name, artist_name, album_name)
 
 
-@st.cache_data()
+@st.cache_data
 def filter_by_market(market):
     if market in ['IL', 'INTL']:
         return glz_df[glz_df['market'] == market]
     else:
         return glz_df
 
-@st.cache_data()
+@st.cache_data
 def circle_image(image_url):
     response = requests.get(image_url).content
     image = Image.open(BytesIO(response))
 
-    # Find the smaller dimension for a square crop
     size = min(image.size)
 
-    # Create a new square image to hold the circle
     mask = Image.new('L', (size, size), 0)
     draw = ImageDraw.Draw(mask)
     draw.ellipse((0, 0, size, size), fill=255)
 
-    # Resize the original image to the square size
     image.thumbnail((size, size))
 
-    # Crop the original image using the square mask
     image = image.crop((0, 0, size, size))
     image.putalpha(mask) 
 
@@ -151,38 +173,22 @@ def plot_scatter_song_length(glz_df):
     return fig
 
 @st.cache_data(show_spinner=False)
-def plot_artist_stats(market):
-    market_data = filter_by_market(market)
+def plot_artist_stats(market, year, rank):
+    market_data = filter_dataframe(
+        glz_df,
+        ('market', market),
+        ('year', year),
+        ('rank', rank)
+    )
+
+    top_ranked_songs = market_data[market_data['rank'] <= rank]
 
     # Calculate the number of weeks each song stays in the top 10
-    market_data['week_count'] = market_data.groupby('track_name')['date'].transform(lambda x: x.nunique())
-
-    # Calculate the total number of appearances for each artist
-    artist_appearances = market_data.groupby('main_artist').size().reset_index(name='count')
-
-    # Get the top 10 artists with the most appearances
-    top_10_artists = artist_appearances.nlargest(10, 'count')['main_artist']
-
-    # Filter the data to include only the top 10 artists
-    top_10_df = market_data[market_data['main_artist'].isin(top_10_artists)]
-
-    # Drop duplicates to get one entry per song per artist
-    duration_df = top_10_df.drop_duplicates(subset=['track_name', 'main_artist'])
-
-    # Calculate the average weeks in top 10 for each artist
-    average_weeks = duration_df.groupby('main_artist').agg({'main_artist_name': 'first', 'week_count': 'mean'}).reset_index()
-    average_weeks.columns = ['URI', 'Artist', 'Average Weeks in Top 10']
-
-    # Calculate the number of different songs each artist has had on the billboard
-    unique_songs = duration_df.groupby('main_artist').agg({'main_artist_name': 'first', 'track_name': 'nunique'}).reset_index()
-    unique_songs.columns = ['URI', 'Artist', 'Number of Different Songs']
-
-    # Merge the dataframes
-    merged_df = pd.merge(average_weeks, unique_songs, on='Artist')
-    merged_df = merged_df.sort_values(by='Number of Different Songs', ascending=False)
+    plot_df = get_artist_song_count(top_ranked_songs)
+    plot_df = plot_df.nlargest(10, 'unique_tracks')
 
     # Create dictionary of artists images
-    uris = top_10_artists.tolist()
+    uris = plot_df['main_artist'].tolist()
     artist_photos = st.session_state['spotify'].get_artists_images(uris)
 
     # Create a scatter plot for the number of different songs and average song time on the billboard
@@ -191,15 +197,17 @@ def plot_artist_stats(market):
     
     # Add dots for average weeks in top 10
     fig.add_trace(go.Scatter(
-        x=merged_df['Artist'],
-        y=merged_df['Number of Different Songs'],
+        x=plot_df['Artist'],
+        y=plot_df['unique_tracks'],
         mode='markers+lines',
         name='Average Weeks in Top 10',
         marker=dict(color='blue', size=10)
     ))
 
+    max_x = plot_df[['unique_tracks', 'ratio']].max().max()
+
     # Add dots for number of different songs with artist photos
-    for index, row in merged_df.iterrows():
+    for index, row in plot_df.iterrows():
         if row['Artist'] in artist_photos:
             photo_url = artist_photos[row['Artist']]
             image = circle_image(photo_url)
@@ -211,9 +219,9 @@ def plot_artist_stats(market):
                     xanchor="center",
                     yanchor="middle",
                     x=row['Artist'],
-                    y=row['Number of Different Songs'],
-                    sizex=3.5,
-                    sizey=3.5,
+                    y=row['unique_tracks'],
+                    sizex=max_x / 6.5,
+                    sizey=max_x / 6.5,
                     sizing="contain",
                     layer="above"
                 )
@@ -221,8 +229,8 @@ def plot_artist_stats(market):
 
     # Add dots for average weeks in top 10
     fig.add_trace(go.Scatter(
-        x=merged_df['Artist'],
-        y=merged_df['Average Weeks in Top 10'],
+        x=plot_df['Artist'],
+        y=plot_df['ratio'],
         mode='markers+lines',
         name='Average Weeks in Top 10',
         marker=dict(color='orange', size=10)
@@ -232,7 +240,7 @@ def plot_artist_stats(market):
     fig.update_layout(
         title=f'Number of Different Songs and Average Song Time in Billboard for Top 10 Artists ({market})',
         xaxis=dict(title='Artist'),
-        yaxis=dict(title='Count / Weeks in Top 10', range=[0, merged_df[['Number of Different Songs', 'Average Weeks in Top 10']].max().max() + 2]),
+        yaxis=dict(title='Count / Weeks in Top 10', range=[0, 1.15 * max_x]),
         template='plotly_white',
         showlegend=True
     )
@@ -240,4 +248,4 @@ def plot_artist_stats(market):
     return fig
 
 st.plotly_chart(plot_scatter_song_length(glz_df))
-st.plotly_chart(plot_artist_stats('all'))
+st.plotly_chart(plot_artist_stats('INTL', 2018, 5))
